b83e1e5c6a3f28681fe6c72648bc495ae292745e
kyle1668 <kyledevinobrien1@gmail.com>
2019-01-08 14:47:32 -0800

    added message model

---
+++ cruzhacks_2019_announcments_service/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cruzhacks_2019_announcments_service.Models
+{
+    public class Message
+    {
+        public string senderName { get; set; }
+        public string timeStamp { get; set; }
+        public string messageTitle { get; set; }
+        public string messageBody { get; set; }
+    }
+}
