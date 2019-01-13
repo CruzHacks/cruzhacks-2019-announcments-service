1bec9a903fbbc7f92c14242e46f02c35d461f901
kyle1668 <kyledevinobrien1@gmail.com>
2019-01-13 13:41:49 -0800

    added db context

---
+++ cruzhacks_2019_announcments_service/Models/MessageContext.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using cruzhacks_2019_announcments_service.Models;
+
+public class MessageContext : DbContext
+{
+	public MessageContext(DbContextOptions<MessageContext> options) : base (options) {}
+    public DbSet<Message> StoredMessages { get; set; }
+}
